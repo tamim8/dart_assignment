7d0cffca2e4474ae58245266c5249d0147336db5
mc-three <tamimh40@gmail.com>
2024-08-07 16:27:22 +0600

    Added some comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'home_page.dart';
 
+/* This is the 2nd assignment of ostad flutter batch seven
+  * Completed by TAMIM HASAN 
+  * Date: 07-08-2024
+  */
+
 void main() {
   runApp(const MyApp());
 }
